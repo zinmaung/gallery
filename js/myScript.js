073b5c8484551b04436ab8c3d3a79a019f8aa54a
zinmaung <zindoublem@gmail.com>
2017-04-26 13:28:47 -0700

    Added new files

---
+++ js/myScript.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    var $container = $('#container').masonry({
+        itemSelector: '.item',
+        columnWidth: 100
+    });
+
+    // reveal initial images
+    $container.masonryImagesReveal($('#images').find('.item'));
+});
+
+$.fn.masonryImagesReveal = function ($items) {
+    var msnry = this.data('masonry');
+    var itemSelector = msnry.options.itemSelector;
+    // hide by default
+    $items.hide();
+    // append to container
+    this.append($items);
+    $items.imagesLoaded().progress(function (imgLoad, image) {
+        // get item
+        // image is imagesLoaded class, not <img>, <img> is image.img
+        var $item = $(image.img).parents(itemSelector);
+        // un-hide item
+        $item.show();
+        // masonry does its thing
+        msnry.appended($item);
+    });
+
+    return this;
+};
